f099603e8aa358d0febf512866169fecd1306cb2
Robbie Cook <robbieck7@gmail.com>
2019-06-28 21:28:20 +1200

    Index page changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,26 @@
-import React, { Component } from 'react';
-import { Heading } from '../components/Typography'
-import {Col, Row, Container as LayoutWrapper} from '../components/Wrappers' 
-import WebPage from '../components/Page'
+import React, { Component } from "react"
+import { Heading } from "../components/Typography"
+import { Col, Row, LayoutWrapper } from "../components/Layout"
+import WebPage from "../components/Page"
+import { BorderBox } from '../components/Boxes';
+import ParliamentGraphic from "../components/politics/ParliamentGraphic"
 
 // Main Page component
 class Index extends Component {
-    render() {
-      return (
-        <WebPage>
-
-        </WebPage>
-      );
-    }
+  render() {
+    return (
+      <WebPage>
+        
+        <LayoutWrapper>
+          <Heading type="h1" center>Parliament</Heading>
+          <Row>
+            <Col><BorderBox><ParliamentGraphic></ParliamentGraphic></BorderBox></Col>
+            <Col><BorderBox>Hi</BorderBox></Col>
+          </Row>
+        </LayoutWrapper>
+      </WebPage>
+    )
   }
-  
-  export default Index;
-  
+}
+
+export default Index

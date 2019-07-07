711ac16aa6e3f6af2b2c4eac35b6ccf789243002
Robbie Cook <robbieck7@gmail.com>
2019-07-08 09:41:19 +1200

    Converted index page to functional component

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,21 +7,28 @@ import ParliamentGraphic from "../components/politics/ParliamentGraphic"
 import MemberPage from "../components/politics/MemberPage"
 
 // Main Page component
-class Index extends Component {
-  render() {
-    return (
-      <WebPage>
-        
-        <LayoutWrapper>
-          <Heading type="h1" center>Parliament</Heading>
-          <Row>
-            <Col><BorderBox><ParliamentGraphic></ParliamentGraphic></BorderBox></Col>
-            <Col><BorderBox>Hi</BorderBox></Col>
-          </Row>
-        </LayoutWrapper>
-      </WebPage>
-    )
-  }
-}
 
-export default Index
+export default function Index(props) {
+  // Render
+  return (
+    <WebPage>
+      <LayoutWrapper>
+        <Heading type="h1" center>
+          Parliament
+        </Heading>
+        <Row>
+          <Col width="50%">
+            <BorderBox style="max-width: 300px; margin: auto;">
+              <ParliamentGraphic></ParliamentGraphic>
+            </BorderBox>
+          </Col>
+          <Col width="50%">
+            <BorderBox>
+            	<MemberPage member={props.currentMember} />
+            </BorderBox>
+          </Col>
+        </Row>
+      </LayoutWrapper>
+    </WebPage>
+  )
+}

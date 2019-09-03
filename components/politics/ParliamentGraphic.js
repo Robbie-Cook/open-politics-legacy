2c0d074d0a83705c26c73abd44f009c1dd23d04c
Robbie Cook <robbieck7@gmail.com>
2019-09-03 19:05:12 +1200

    Added activeMember prop to ParliamentGraphic
    This is so ParliamentGraphic knows which seat to animate (preferably with framer-motion)

--- components/politics/ParliamentGraphic.js
+++ components/politics/ParliamentGraphic.js
@@ -2,6 +2,7 @@ import { Transition } from "@robbie-cook/react-components"
 import React from "react"
 import { Circle, Layer, Stage } from "react-konva"
 import seatingData from "./data/seatingData"
+import PropTypes from "prop-types"
 
 // ParliamentGraphic component
 function ParliamentGraphic({ callback }) {
@@ -36,6 +37,11 @@ ParliamentGraphic.defaultProps = {
   callback: () => {
     console.log("Default clicked")
   },
+  activeMember: 0,
+}
+ParliamentGraphic.propTypes = {
+  activeMember: PropTypes.number,
+  callback: PropTypes.func,
 }
 
 export default ParliamentGraphic

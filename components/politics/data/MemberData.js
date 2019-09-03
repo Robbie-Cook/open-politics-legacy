154d4483e3e8582cc3d48c7a88267ee9e218b8d4
Robbie Cook <robbieck7@gmail.com>
2019-09-03 19:01:09 +1200

    Added MemberPageImage and member data object

---
+++ components/politics/data/MemberData.js
@@ -0,0 +1,45 @@
+import Prismic from "prismic-javascript"
+
+/**
+ * Turn an array to an object in case so that the keys
+ * don't have to be sequential e.g. {0: {}, 2: {}, 3: {}}
+ *
+ * @param {array} array The array to sort
+ * @param {string} key The key of the object in the array to
+ * use. If this is not provided, then the index is used
+ */
+const mapToKeyedObject = (array, key = null) => {
+  let object = {}
+  array.forEach((element, index) => {
+    let myKey = key ? element[key] : index
+    object[myKey] = element
+  })
+  return object
+}
+
+/**
+ * Get all member data for all members.
+ *
+ * @param {string} apiEndpoint The string endpoint to call
+ *
+ * @return {object} An keyed object of members by UID
+ */
+export const getMemberData = apiEndpoint => {
+  return Prismic.getApi(apiEndpoint)
+    .then(api => {
+      return api.query("") // Need to query by member type
+    })
+    .then(response => {
+      let data = mapToKeyedObject(response.results, "uid")
+      return data
+    })
+}
+
+/**
+ * Get a member from a member array
+ * @param {object} members 
+ * @param {string} memberId 
+ */
+export const getMember = (members, memberId) => {
+  return members[memberId].data
+}
